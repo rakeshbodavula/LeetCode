a196f73605235d0008a842b01588828c47e50828
Rakesh <rakeshbodavula2002@gmail.com>
2021-10-13 13:26:48 +0530

    Merge Sorted Array

---
+++ 88.cpp
@@ -0,0 +1,15 @@
+// problem link: https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k--]=nums1[i--];
+            }
+            else nums1[k--]=nums2[j--];
+        }
+        for(int h=j;h>=0;h--) nums1[h]=nums2[h];
+    }
+};
