bfadb1264e5f72eae3224156b6be5c7249291dec
Rakesh <rakeshbodavula2002@gmail.com>
2021-11-06 17:32:56 +0530

    First Bad Version

---
+++ 278.cpp
@@ -0,0 +1,14 @@
+// problem link : https://leetcode.com/problems/first-bad-version/
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1,r=n,mid;
+        while(l<=r){
+            mid=l+(r-l)/2;
+            if(isBadVersion(mid)) r=mid-1;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
