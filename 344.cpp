bbb2c887463c5dbda5be56bedb6060e82458b846
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2021-11-12 19:32:48 +0530

    Reverse String

---
+++ 344.cpp
@@ -0,0 +1,11 @@
+// problem link : https://leetcode.com/problems/reverse-string/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        for(int i=0;i<(n+1)/2;i++){
+            swap(s[i],s[n-i-1]);
+        }
+    }
+};
