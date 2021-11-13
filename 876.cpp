77d2b6228cc84b3e8de70f7a0f2a54a94d9b9c71
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2021-11-13 13:49:04 +0530

    Middle of the Linked List

---
+++ 876.cpp
@@ -0,0 +1,15 @@
+// problem link : https://leetcode.com/problems/middle-of-the-linked-list/
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* tmp=head;
+        ListNode*ptr=head;
+        while(ptr->next && ptr->next->next){
+            tmp=tmp->next;
+            ptr=ptr->next->next;
+        }
+        if(ptr->next) return tmp->next;
+        return tmp;
+    }
+};
